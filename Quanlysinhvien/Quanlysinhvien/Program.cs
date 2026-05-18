b211dd3f636965683b38302d3461dee19748f502
AnhVuu215 <anhvuvan20052005@gmail.com>
2026-05-18 22:54:23 +0700

    Hoan thanh giao dien QLSV va tao moi form Quan ly Lop hoc

--- Quanlysinhvien/Quanlysinhvien/Program.cs
+++ Quanlysinhvien/Quanlysinhvien/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LoginForm
+namespace Quanlysinhvien
 {
     internal static class Program
     {
@@ -16,7 +16,7 @@ namespace LoginForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new QLSV());
-		}
+            Application.Run(new Form1());
+        }
     }
 }
